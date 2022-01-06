2ef5dd94cdd7844ec8a443d0e260657b6ef750f9
lenryk <lenry101@gmail.com>
2022-01-06 18:03:38 +0000

    first commit

---
+++ chapter-1/1-1-ECMA-script/exercises.js
@@ -0,0 +1,5 @@
+// 1. What are the two ways to ensure your code is compatiable in most browsers?
+// Visit caniuse.com to check the compatability across multiple browsers
+
+// 2. What is the difference between ECMAScript and JavaScript
+// ECMA Script is a general purpose scripting language. JavaScript is an implementation of the ECMAScript specification
