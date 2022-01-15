cbfa48721fa1c2afb326197f1e42d555a358280b
lenryk <lenry101@gmail.com>
2022-01-15 11:08:01 +0000

    finished object exercises

---
+++ chapter-3/3-1-objects/exercises.js
@@ -0,0 +1,15 @@
+// 1. What other key: value pairs can you think of (e.g. credit card numbers : pin)
+
+// Dictionary (word : definition)
+// Postcode : address
+// License plate : car model
+// Drivers license numbers : drivers info
+
+// 2. What is a method?
+
+// A method is a function that is part of an object
+
+// 3. What is the difference between copy by value and copy by reference?
+
+// Copy by value is what primitive data types do e.g. numbers, string and make a copy of the value itself
+// Copy by reference is where we send the memory location of the data and is only used on objects like arrays and functions
