2f8d70ba59b65d2a1269816e005a66213e1d7ef2
lenryk <lenry101@gmail.com>
2022-02-09 18:49:08 +0000

    finished DOM exercises

---
+++ chapter-7/7-1-what-is-the-dom/exercises.js
@@ -0,0 +1,15 @@
+// 1. Can you explain the DOM?
+
+// Its an API that we can use to control the objects on the web page
+
+// 2. What are the 2 purposes of the DOM?
+
+// To render and modify items
+
+// 3. The DOM API can only be used with JavaScript true or false?
+
+// False, you can use it with any language
+
+// 4. How are objects in the DOM assembled?
+
+// They are assembled with the hierarchy model as the document objects holds all the children
